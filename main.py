31e131da3f5c9f761deb8e6ca2981edea6e09065
tusharjoshi948@gmail.com <tusharjoshi948@gmail.com>
2021-04-22 16:05:14 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+calculation_to_units = 24
+name_of_unit = "hours"
+
+def days_to_units():
+    print(f"20 days are {20 * calculation_to_units} {name_of_unit}")
+    print(f"40 days are {40 * calculation_to_units} {name_of_unit}")
+    print(f"60 days are {60 * calculation_to_units} {name_of_unit}")
+    print(f"80 days are {80 * calculation_to_units} {name_of_unit}")
+    print(f"100 days are {100 * calculation_to_units} {name_of_unit}")
+    print("All Good")
+
+
+days_to_units()
+
+
+
